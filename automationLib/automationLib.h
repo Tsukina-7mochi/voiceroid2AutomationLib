adfbbb101fe32640a5ac833be53f88520a0d4a3d
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2021-01-24 05:52:38 +0900

    コメントを追加

--- automationLib/automationLib.h
+++ automationLib/automationLib.h
@@ -1,10 +1,49 @@
 #pragma once
 
 #include <Windows.h>
+
+
+// ===== 統合機能 =====
+
+// テキストを読み上げ
+// @param
+//   message: 読み上げるテキスト
+// @returns
+//   実行結果
 extern "C" __declspec(dllexport) int talk(LPCWSTR message);
+
+// 読み上げを停止(停止ボタンを押下)
+// @returns
+//   実行結果
 extern "C" __declspec(dllexport) int stopTalk();
+
+
+// ===== 個別機能 =====
+
+// テキストを設定
+// @param
+//   message: 設定するテキスト
+// @returns
+//   実行結果
 extern "C" __declspec(dllexport) int setTextView(LPCWSTR message);
+
+// TextViewにあるボタンを押下
+// @param
+//   text: ボタンのテキスト
+// @returns
+//   実行結果
 extern "C" __declspec(dllexport) int pushTextViewButton(LPCWSTR text);
+
+// SleepModeOnボタンを押下
+// @returns
+//   実行結果
 extern "C" __declspec(dllexport) int sleepMode();
+
+
+// ===== デバッグ =====
+
+// デバッグを有効化
 extern "C" __declspec(dllexport) void enableDebug();
+
+// デバッグを無効化
 extern "C" __declspec(dllexport) void disableDebug();
--- automationLib/controlEditor.h
+++ automationLib/controlEditor.h
@@ -6,6 +6,7 @@
 #include <propvarutil.h>
 #include <stdio.h>
 
+// 実行結果
 enum class CtrlEditResult : int {
 	succeeded = 0,
 	init,
@@ -20,9 +21,29 @@ enum class CtrlEditResult : int {
 	getLabel
 };
 
+// デバッグが有効かどうか
 extern bool ctrlEditDebug;
 
+
+
+// COMを初期化
+// @returns
+//   操作の成否
 extern HRESULT InitializeCOM();
+
+// テキストボックスに文字列を設定
+// @param
+//   message: 設定する文字列
+//   hWnd   : VOICEROID2 Editorのウィンドウハンドル
 extern CtrlEditResult SetText(LPCWSTR message, HWND hWnd);
+
+// カスタム #c 内の指定した名前のテキストが子要素に存在するボタンを押下
+// @param
+//   message: ボタンの文字列
+//   hWnd   : VOICEROID2 Editorのウィンドウハンドル
 extern CtrlEditResult PushCButton(LPCWSTR buttonText, HWND hWnd);
+
+// Sleep Mode をOnにする
+// @param
+//   hWnd     : VOICEROID2 Editorのウィンドウハンドル
 CtrlEditResult PushSleepModeOnButton(HWND hWnd);
